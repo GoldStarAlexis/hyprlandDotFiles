a0dbdf1e461271e0490d3bee6f24aecb869a08bf
Alexis Dunn <alexissss964@gmail.com>
2025-03-25 21:41:51 -0400

    Quick update v2

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Tide Configuration
 
+set -x HSA_OVERRIDE_GFX_VERSION 11.0.0
+set -x HIP_VISIBLE_DEVICES 0
+set -x ROCR_VISIBLE_DEVICES 0
+set -x PATH $PATH /opt/rocm/bin /opt/rocm/opencl/bin
