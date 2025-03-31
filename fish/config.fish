7307667b5671dad72c19d1a7e69d55265f985220
Alexis Dunn <alexissss964@gmail.com>
2025-03-31 09:04:31 -0400

    Update for my sanity to make sure it's backed up

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,6 @@ set -x HSA_OVERRIDE_GFX_VERSION 11.0.0
 set -x HIP_VISIBLE_DEVICES 0
 set -x ROCR_VISIBLE_DEVICES 0
 set -x PATH $PATH /opt/rocm/bin /opt/rocm/opencl/bin
+
+set -x OLLAMA_FLASH_ATTENTION 1
+set -x OLLAMA_KV_CACHE_TYPE q8_0
